d4bb9629351129b0a464dee6243df6dcf3d302e6
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-14 19:11:57 -0700

    even or odd

---
+++ drills.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function isEven(value){
+  if (value % 2 == 0){
+    return true;
+  }
+  else
+    return false;
+}
+
+// Even or Odd
+// O(1) 
+// because it is one of two answers that are found in a one-step check
