8e491ab4578ec764da313ba2a6d504ffa4cc273b
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-15 08:52:00 -0700

    everything up to is prime

--- drills.js
+++ drills.js
@@ -9,5 +9,165 @@ function isEven(value){
 }
 
 // Even or Odd
-// O(1) 
+// O(1) - constant
 // because it is one of two answers that are found in a one-step check
+
+
+
+
+function areYouHere(arr1, arr2) {
+  for (let i=0; i<arr1.length; i++) {
+      const el1 = arr1[i];
+      for (let j=0; j<arr2.length; j++) {
+          const el2 = arr2[j];
+          if (el1 === el2) return true;
+      }
+  }
+  return false;
+}
+
+// Are you here?
+// O(n^2) - polynomial
+// Because the nested loop creates an exponential data return, but only to the second degree
+
+
+
+
+function doubleArrayValues(array) {
+  for (let i=0; i<array.length; i++) {
+      array[i] *= 2;
+  }
+  return array;
+}
+
+// Doubler
+// O(n) - linear
+// Because you go through each item in the array one at a time
+
+
+
+
+function naiveSearch(array, item) {
+  for (let i=0; i<array.length; i++) {
+      if (array[i] === item) {
+          return i;
+      }
+  }
+}
+
+// Naive Search
+// O(n) - linear
+// Because you go through each item in the array, one at a time, until you get to item
+
+
+
+
+
+function createPairs(arr) {
+  for (let i = 0; i < arr.length; i++) {
+      for(let j = i+1; j < arr.length; j++) {
+          console.log(arr[i] + ", " +  arr[j] );
+      }
+  }
+}
+
+// Creating pairs
+// O(n^2) - polynomial
+// because there is one nested loop
+
+
+
+
+function generateFib(num) {
+  let result = [];
+  for (let i = 1; i <= num; i++) {
+
+    // we're adding the first item
+    // to the result list, append the
+    // number 0 to results
+    if (i === 1) {
+      result.push(0);
+    }
+    // ...and if it's the second item
+    // append 1
+    else if (i == 2) {
+      result.push(1);
+    }
+
+    // otherwise, sum the two previous result items, and append that value to results.
+    else {
+      result.push(result[i - 2] + result[i - 3]);
+    }
+  }
+  // once the for loop finishes
+  // we return `result`.
+  return result;
+}
+
+// Computing fibonaccis
+// O(n) - linear
+// Because you go through the numbers one at a time, adding the first to the subsequent number and returning it.
+
+
+
+
+
+
+function efficientSearch(array, item) {
+  let minIndex = 0;
+  let maxIndex = array.length - 1;
+  let currentIndex;
+  let currentElement;
+
+  while (minIndex <= maxIndex) {
+      currentIndex = Math.floor((minIndex + maxIndex) / 2);
+      currentElement = array[currentIndex];
+
+      if (currentElement < item) {
+          minIndex = currentIndex + 1;
+      }
+      else if (currentElement > item) {
+          maxIndex = currentIndex - 1;
+      }
+      else {
+          return currentIndex;
+      }
+  }
+  return -1;
+}
+
+// An Efficient Search
+// O(logn) - logarithmic 
+// Because you are able to knock out half the data set making it pretty darn efficient
+
+
+
+
+function findRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+// Random element
+// O(1) - constant
+// it returns a random item from the array in a one step process
+
+
+
+
+function isPrime(n) {
+  // if n is less than 2 or a decimal, it's not prime
+  if (n < 2 || n % 1 != 0) {
+      return false;
+  }
+  // otherwise, check if `n` is divisible by any integer
+  // between 2 and n.
+  for (let i = 2; i < n; ++i) {
+      if (n % i == 0) return false;
+  }
+  return true;
+}
+
+// Is it prime?
+// O(n) - linear
+// Because it has to check if n meets the conditions, one increment at a time, until it finds the right answer
+
