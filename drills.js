0f1cd7785fc04d568f57d883b1eebca4dff47b14
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-15 09:33:16 -0700

    up to anagrams

--- drills.js
+++ drills.js
@@ -143,6 +143,7 @@ function efficientSearch(array, item) {
 
 
 
+
 function findRandomElement(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
@@ -150,6 +151,7 @@ function findRandomElement(arr) {
 // Random element
 // O(1) - constant
 // it returns a random item from the array in a one step process
+
 
 
 
@@ -171,3 +173,179 @@ function isPrime(n) {
 // O(n) - linear
 // Because it has to check if n meets the conditions, one increment at a time, until it finds the right answer
 
+
+
+
+// =====================
+// Recursive Exercises
+// =====================
+
+
+function countSheep(num){
+  //stopping condition of base case
+  if(num === 0){
+      console.log(`All sheep jumped over the fence`);
+  } 
+  //this is the recursive case
+  //this will be executed until it reaches base case
+  else{
+      console.log(`${num}: Another sheep jump over the fence`);
+      countSheep(num-1);
+  }
+}
+
+// Counting Sheep
+// O(n) - linear
+// Because it counts down sheep, one at a time, until there are no more sheep
+
+
+
+
+
+function double_all(arr) {
+  if (!arr.length) {
+      return [];
+  }
+  return [arr[0] * 2, ...double_all(arr.slice(1))];
+}
+
+// Array Double
+// O(n) - linear
+// Because it goes through each item in the array one at a time
+
+
+
+
+
+
+function reverseString(str) {
+  if (str.length < 2) {
+      return str;
+  }
+  return reverseString(str.slice(1)) + str[0];
+}
+
+// Reverse String
+// O(n) - linear
+// Because it goes through each letter in the string one at a time
+
+
+
+
+
+
+function triangle(n) {
+  if (n < 2) 
+      return n;
+  return n + triangle(n - 1);
+}
+
+// Triangular Number
+// O(n) - linear
+// Because you go through each row in the triangle, one at a time
+
+
+
+
+
+function split(str, sep) {
+  var idx = str.indexOf(sep);
+  if (idx == -1) 
+      return [str];
+  //you don't have to return an array, you can return a string as an array of 
+  //character 
+  //return str;
+  return [str.slice(0, idx)].concat(split(str.slice(idx + sep.length), sep))
+//all these are valid syntax
+//return (str.slice(0,idx) + (split(str.slice(idx + sep.length), sep)))
+//return str.slice(0,idx).concat(split(str.slice(idx + sep.length), sep))
+}
+
+// String Splitter
+// O(logn) - logarithmic
+// Because it splits the data making it decently efficient
+
+
+
+
+
+
+function convertToBinary(num){
+  if(num>0){
+      let binary = Math.floor(num%2); //save the reminder in binary
+  //divide the number by 2 and send that to the function again
+  //carry the reminder to the next recursion call
+      return (convertToBinary(Math.floor(num/2))+ binary);
+  }else{
+      return ''; //base case - at some point the divisions will lead to 0
+  }
+}
+
+// Binary Representation 
+// O(logn) - logarithmic
+// Because you divide by two, this makes a large data set only a little bit more inefficient than a small data set
+// ****
+
+
+
+
+function factorial(n) {  
+  // Base Case - when n is equal to 0, we stop the recursion
+  if (n === 0) {
+    return 1;
+  }
+  // This is our Recursive Case
+  // It will run for all other conditions except when n is equal to 0
+  return n * factorial(n - 1);
+}
+
+// Factorial
+// O(n) - linear
+// Because it decrements n, one at a time until n is 0
+
+
+
+
+
+function fibonacci(n) {
+  // Base case
+  if (n <= 0) {
+    return 0;
+  }
+  // Base case
+  if (n <= 2) {
+    return 1;
+  }	
+  // Recursive case
+  return fibonacci(n - 1) + fibonacci(n - 2);	
+}
+
+// Fibonacci
+// O(n) - linear
+// Because it goes through the fibonacci sequence by creating one number in the sequence at a time.
+// ****
+
+
+
+
+function anagrams(prefix, str){
+  if(str.length <= 1){
+      console.log(`The anagram is ${prefix}${str}`);
+  } else {
+      for(let i=0; i<str.length; i++){
+          let currentLetter = str.substring(i, i+1); 
+          let previousLetters = str.substring(0,i);
+          let afterLetters = str.substring(i+1);
+          anagrams(prefix+currentLetter, previousLetters+afterLetters);
+      }
+  }
+}
+function printAnagram(word){
+  //console.log(`The word for which we will find an anagram is ${word}`);
+  anagrams(' ', word);
+
+}
+
+// Anagrams
+// o(2^n) - exponential
+// Because you have recursion within a loop
